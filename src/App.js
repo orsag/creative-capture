53a2c3a8a5b62fedb6e758cf258622c6e2daf1f7
Martin Orsag <martin.orsag108@gmail.com>
2021-01-01 14:30:58 +0100

    About styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import pages
 import AboutUs from './pages/AboutUs'
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
     <div className="App">
-      <AboutUs />
+        <GlobalStyle />
+        <AboutUs />
     </div>
   );
 }
