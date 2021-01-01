c3ea6169944cea8a17c177c8d0ded8c2c8337cfc
Martin Orsag <martin.orsag108@gmail.com>
2021-01-01 18:31:44 +0100

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import pages
 import AboutUs from './pages/AboutUs'
+import Navigation from "./components/Navigation"
 import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle />
+        <Navigation />
         <AboutUs />
     </div>
   );
