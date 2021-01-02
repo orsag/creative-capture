2ab533cdb0213a4461f7863491d7f2cb8b37b959
Martin Orsag <martin.orsag108@gmail.com>
2021-01-02 11:28:52 +0100

    OurWork, MovieDetail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 //import pages
 import AboutUs from './pages/AboutUs'
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from './pages/MovieDetail'
 import Navigation from "./components/Navigation"
 import GlobalStyle from './components/GlobalStyle'
+// Router
+import { Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-        <GlobalStyle />
-        <Navigation />
-        <AboutUs />
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <Navigation />
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs />
+                </Route>
+                <Route path="/contact">
+                    <ContactUs />
+                </Route>
+                <Route path="/work" exact>
+                    <OurWork />
+                </Route>
+                <Route path="/work/:id">
+                    <MovieDetail />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
